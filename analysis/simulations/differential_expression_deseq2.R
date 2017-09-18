4a73b2da338c60e735ba522e290418fee2d96a51
Kieran Campbell <kieranrcampbell@gmail.com>
2017-09-18 10:49:04 +0200

    deseq exception handling

--- analysis/simulations/differential_expression_deseq2.R
+++ analysis/simulations/differential_expression_deseq2.R
@@ -21,13 +21,18 @@ dex_analysis_deseq2 <- function(input_sceset = "sce.rds",
   count_mat <- counts(sce[, cells_non_na])
   coldata = pData(sce[, cells_non_na])
   
-  dds <- DESeqDataSetFromMatrix(countData = count_mat,
-                                colData = coldata, 
-                                design = ~ pseudotime + x + pseudotime:x)
-  dds <- DESeq(dds)
-  res <- results(dds)
-  
-  interaction_qval <- res$padj
+  interaction_qval <- tryCatch({
+    dds <- DESeqDataSetFromMatrix(countData = count_mat,
+                                  colData = coldata, 
+                                  design = ~ pseudotime + x + pseudotime:x)
+    dds <- DESeq(dds)
+    res <- results(dds)
+    interaction_qval <- res$padj
+    interaction_qval
+  },
+  error = function(cond) {
+    return(rep(NA, nrow(sce)))
+  })
   
   output_data_frame <- data_frame(qval = interaction_qval)
   write_csv(output_data_frame, output_file)
