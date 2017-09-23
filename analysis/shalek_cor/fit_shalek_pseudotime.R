692a5abccacd30139bc8da9464a7933837ea26d6
Kieran Campbell <kieranrcampbell@gmail.com>
2017-09-23 11:38:04 +0100

    fml

--- analysis/shalek_cor/fit_shalek_pseudotime.R
+++ analysis/shalek_cor/fit_shalek_pseudotime.R
@@ -10,7 +10,7 @@ library(dpt)
 
 fit_phenopath <- function(exprs_mat, x, pst_init = 1) {
   fit <- phenopath(t(exprs_mat), x, z_init = pst_init,
-                   a_beta = 1e-2, b_beta = 1e-2)
+                   a_beta = 1e2, b_beta = 1e-2)
   return(fit)
 }
 
