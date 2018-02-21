e458098beafec671f48d3f54a9cfd1f4aa633df7
Kieran Campbell <kieranrcampbell@gmail.com>
2018-02-21 12:39:43 -0800

    Even more stupid than before

--- analysis/dropout/phenopath_inference.R
+++ analysis/dropout/phenopath_inference.R
@@ -20,9 +20,9 @@ pseudotime_inference <- function(input_file = "sceset.rds",
   true_pseudotimes <- sce$pst
   abs_kendall <- abs(cor(pst, true_pseudotimes, method = "kendall", use = "na"))  
   
-  original_prop_zero <- sce$original_prop_zero
-  new_prop_zero <- sce$new_prop_zero
-  proportion_set_to_zero <- sce$proportion_set_to_zero
+  original_prop_zero <- sce$original_prop_zero[1]
+  new_prop_zero <- sce$new_prop_zero[1]
+  proportion_set_to_zero <- sce$proportion_set_to_zero[1]
   
   output_df <- data.frame(input_sceset = input_file,
                           kendall_correlation = abs_kendall,
